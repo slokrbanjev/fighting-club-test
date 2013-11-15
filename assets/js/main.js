5f60471717924510d30037fcfacdd53a33fc0796
Lukas Jakob Hafner <luki@booncon.com>
2013-11-15 11:26:47 +0200

    rework on a lot of things, new textures, photos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
-  $('.main-menu a, #intro a, #join a, .sign-up a').click(function (e) {
+  $('.main-menu a').click(function (e) {
   	e.preventDefault();
     $('body').animate({
       scrollTop: $($(this).attr('href')).offset().top
     }, 600);
+  });
+  $(document).on('scroll', function () {
+    if ($(this).scrollTop() > 100) {
+      $('.glove').addClass('punch');
+    } else {
+      $('.glove').removeClass('punch');
+    }
+  });
+  $('.gallery').slippry({
+    transition: 'kenburns',
+    easing: 'linear',
+    speed: 2000,
+    pause: 6000,
+    kenZoom: 260
   });
 });
