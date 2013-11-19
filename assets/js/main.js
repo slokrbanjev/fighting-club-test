8fa04618aa5043d60eb792920bfdf0a9328e4e71
Lukas Jakob Hafner <luki@booncon.com>
2013-11-19 18:43:13 +0200

    very fine media queries, mean menu, css refinements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,37 @@
 $(document).ready(function () {
-  $('.main-menu a, .smooth-link').click(function (e) {
+
+  $('.main-menu').meanmenu({
+    // onePage: true,
+    meanScreenWidth: 600
+  });
+  $('.main-menu a, .smooth-link, .mean-nav a').click(function (e) {
   	e.preventDefault();
     $('body').animate({
       scrollTop: $($(this).attr('href')).offset().top
     }, 600);
   });
-  $(document).on('scroll', function () {
-    if ($(this).scrollTop() > 100) {
-      $('.glove').addClass('punch');
-    } else {
-      $('.glove').removeClass('punch');
-    }
-  });
+
+
+  if ($('.glove').css('transitionDuration') !== '0s') {
+    $(document).on('scroll', function () {
+      if ($(this).scrollTop() > 100) {
+        $('.glove').addClass('punch');
+      } else {
+        $('.glove').removeClass('punch');
+      }
+    });
+  }
+  if ($('.day-toggle').css('display') !== 'none') {
+    $('.day_header').click(function () {
+      $('.slots', $(this).closest('.day')).slideToggle();
+      if ($('.day-toggle', $(this).closest('.day')).text() === '⬇︎') {
+        $('.day-toggle', $(this).closest('.day')).text('⬆︎');
+      } else {
+        $('.day-toggle', $(this).closest('.day')).text('⬇︎');
+      }
+    });
+  }  
+
   $('.gallery').slippry({
     transition: 'kenburns',
     easing: 'linear',
